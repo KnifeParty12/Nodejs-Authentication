78399c6df07638bfb1ca1773c4342ba968462437
KnifeParty12 <flashfunk01@gmail.com>
2019-04-14 19:29:34 +0530

    Basic Login-Signup setup
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

---
+++ db.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+
+const db = new Sequelize(
+    'userdb',
+    'userdb',
+    'userdb',
+    {
+        dialect: 'mysql',
+        host: 'localhost'
+    }
+    );
+
+const Users = db.define('users',{
+    username:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    firstName: Sequelize.STRING,
+    lastname: Sequelize.STRING
+});
+
+db.sync().then(()=> console.log("Database is ready"))
+
+exports = module.exports = {
+    db,Users
+};
--- server.js
+++ server.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.listen(1234,()=> console.log("Server listening on http://locallhost:1234/"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'hbs');
+app.set('views','views');
+
+app.use('/public', require('./routes/public'));
+app.use('/private', require('./routes/private'));
+app.use('/',require('./routes/root'));
+app.listen(1234,()=> console.log("Server listening on http://localhost:1234"));
